12c2648f28302ea08b624b1e2a15ab62abc0198c
NicWille <nic.wille1020@yahoo.com>
2021-09-19 09:56:42 -0700

    style cards

--- README.md
+++ README.md
@@ -14,8 +14,7 @@ SO THAT I can plan a trip accordingly
 ## acceptance criteria
 
 ### GIVEN a weather dashboard with form inputs
-1. WHEN I search for a city
-- THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 2. WHEN I view current weather conditions for that city
 - THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 3. WHEN I view the UV index
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,20 +18,23 @@ function useFetch(geoCoordinates) {
     coordinates = ''
     fetch(geoCoordinates)
     .then((resp) => {
+
       if (!resp.ok) throw new Error(resp.statusText);
+
       return resp.json();
     })
     .then((data) => {
-      // console.log(data)
+
       createStoredButton(data)
       lat = data[0].lat
       lon = data[0].lon
       let weatherInfo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=${api}`
       fetch(weatherInfo).then((res) => {
+
           return res.json()
       })
       .then((data) => {
-          // console.log(data)
+
         showCurrentWeather(data)
       })
     })
@@ -58,9 +61,7 @@ function createStoredButton(data) {
   if (!localStorage.getItem(cityName)) {
 
     localStorage.setItem(cityName, cityName)
-    allButtonsEl = allButtonsEl + `<button class="aside-element pure-button pure-button-secondary" type="button">
-    ${cityName}
-    </button>`
+    allButtonsEl = allButtonsEl + `<button class="aside-element pure-button pure-button-secondary" type="button">${cityName}</button>`
     buttonListEl.innerHTML = allButtonsEl
   }
 }
@@ -71,10 +72,7 @@ function populateButtonList() {
   for (var i = 0; i < localStorage.length; i++){
 
     localStorageCity = localStorage.getItem(localStorage.key(i))
-    // console.log(localStorageCity)
-    allButtonsEl = allButtonsEl + `<button class="aside-element pure-button pure-button-secondary" type="button">
-    ${localStorageCity}
-    </button>`
+    allButtonsEl = allButtonsEl + `<button class="aside-element pure-button pure-button-secondary" type="button">${localStorageCity}</button>`
     buttonListEl.innerHTML = allButtonsEl
   } 
 }
@@ -107,7 +105,6 @@ function showFutureWeather(data) {
 
     futureWeatherEl.classList.remove("hidden")
     let cards = ''
-
     for (let i=0; i<5; i++) {
 
       let day = dayjs().add(i + 1, 'day').format('MMM-D-YYYY')
@@ -115,28 +112,18 @@ function showFutureWeather(data) {
       let temp = data.daily[i].temp.day
       let wind = data.daily[i].wind_speed
       let hum = data.daily[i].humidity
-
-      // sty;e cards
- 
-      
       let card = 
-      `<div class="pure-u-lg-1-5">
+      `<div class="pure-u-3-5 pure-u-lg-3-24 glass">
       <h3>${day}</h3>
       <img src="http://openweathermap.org/img/wn/${symbol}.png"/>
       <p>Temp:  ${temp}°F</p>
       <p>Wind:  ${wind} MPH</p>
-      <p>Humidity:  ${hum}%</p>
-      </div>
-      `
+      <p>Humidity:  ${hum}%</p></div>`
       cards = cards + card
-
-
-    }
-    
+    } 
     cardListEl.innerHTML = cards
 }
 
 populateButtonList()
 searchButtonEl.addEventListener("click", handleSearchButton)
 buttonListEl.addEventListener("click", handleStoredButton)
-
